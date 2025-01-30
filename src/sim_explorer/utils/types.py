840f6b08927a05e16647c8d24f883900a83a48b2
Claas <claas.rostock@dnv.com>
2025-01-30 15:32:09 +0100

    src/sim_explorer/utils/types.py : cleaned up

--- src/sim_explorer/utils/types.py
+++ src/sim_explorer/utils/types.py
@@ -1,19 +1,17 @@
-from collections.abc import Callable, Mapping, Sequence
+from collections.abc import Sequence
 from typing import (
     TypeAlias,
 )
 
 # ===== Arguments (Variables) =====================================================================
 TNumeric: TypeAlias = int | float
-TValue: TypeAlias = (
-    int | float | bool | str  # | Sequence[int | float | bool | str]
-)  # single (scalar) value, as e.g. also serialized to/from Json5 atom
+TValue: TypeAlias = int | float | bool | str  # single (scalar) value, as e.g. also serialized to/from Json5 atom
 TTimeColumn: TypeAlias = list[int] | list[float]  # X column, but limited to numeric types. Typically indicating time.
 TDataColumn: TypeAlias = list[int] | list[float] | list[bool]  # | list[str]  # X column
 TDataRow: TypeAlias = Sequence[TValue | Sequence[TValue]]  # | TNumeric  # X row without variable names (just values)
 TDataTable: TypeAlias = Sequence[TDataRow]  # X table
-TArguments: TypeAlias = Mapping[str, TValue]  # X row with variable names
-TArgs: TypeAlias = dict[str, TValue]
+# TArguments: TypeAlias = Mapping[str, TValue]  # X row with variable names  # noqa: ERA001
+# TArgs: TypeAlias = dict[str, TValue]  # noqa: ERA001
 
 
 # ===== System Interface =====================================================================
@@ -23,22 +21,3 @@ TGetActionArgs: TypeAlias = tuple[str, str, tuple[int, ...]]
 TSetActionArgs: TypeAlias = tuple[str, str, tuple[int, ...], tuple[TValue, ...]]
 #: Arguments for action functions
 TActionArgs: TypeAlias = TGetActionArgs | TSetActionArgs
-
-#: Function signature for action functions
-TActionFunc: TypeAlias = Callable[[int | float, TActionArgs, type], bool]
-#: Function signature for action step functions
-TActionStepFunc: TypeAlias = Callable[[TActionArgs, type], Callable[..., TValue]]
-
-#: Function signature for initial action functions
-TInitialActionFunc: TypeAlias = Callable[[int | float, TSetActionArgs], bool]
-#: Function signature for initial action functions
-TInitialActionStepFunc: TypeAlias = Callable[[TSetActionArgs], Callable[..., TValue]]
-
-#: Function signature for get action functions
-TGetActionFunc: TypeAlias = Callable[[TGetActionArgs], TValue]
-#: Function signature for set action functions
-TSetActionFunc: TypeAlias = Callable[[TSetActionArgs], bool]
-#: Function signature for get action step functions
-TGetActionStepFunc: TypeAlias = Callable[[TGetActionArgs], Callable[..., TValue]]
-#: Function signature for set action step functions
-TSetActionStepFunc: TypeAlias = Callable[[TSetActionArgs], Callable[..., TValue]]
